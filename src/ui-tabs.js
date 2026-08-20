36b2b3e1f83dadab55c4d5ddba881dd73bc04009
Min Thant Htoo <minthanthtoo.cs@gmail.com>
2026-08-20 17:57:07 +0630

    feat: convert wizard tech stack selectors to text comboboxes

--- src/ui-tabs.js
+++ src/ui-tabs.js
@@ -580,9 +580,20 @@ function renderWizardDetails() {
     }));
 
     const mkTechSelect = (label, key, options) => {
-      const sel = el("select", { className: "aisq-select" }, [el("option", { value: "", text: `-- Select ${label} --` }), ...options.map(o => el("option", { value: o, text: o, selected: inferred[key] === o }))]);
-      sel.addEventListener("change", e => { ctx.mutate(() => { ctx.state.ui.specAnswers[key] = e.target.value }); ctx.requestRender(); });
-      return field(label, sel);
+      const listId = `aisq-list-${key}`;
+      const dl = el("datalist", { id: listId }, options.map(o => el("option", { value: o })));
+      const input = el("input", { 
+        className: "aisq-input", 
+        type: "text", 
+        list: listId, 
+        placeholder: `Select or type...`,
+        value: inferred[key] && inferred[key] !== "None" ? inferred[key] : "" 
+      });
+      input.addEventListener("change", e => {
+        ctx.mutate(() => { ctx.state.ui.specAnswers[key] = e.target.value || "None" });
+        ctx.requestRender();
+      });
+      return field(label, el("div", { style: "position: relative;" }, [input, dl]));
     };
 
     let techStackFields = null;
